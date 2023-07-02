5e05c7dff5c369003a851712b7f567e215122f2f
Lucas Santos <lucas.silva1203@outlook.com>
2023-07-02 16:39:55 -0300

    test: add imageController test cases

---
+++ src/tests/controllers/imageController.test.ts
@@ -0,0 +1,25 @@
+import supertest from "supertest";
+
+describe("Image Controller - User: ", () => {
+  const endpoint = "http://localhost:6060/api/image";
+
+  test("Upload should return validation error if no data has been uploaded", async () => {
+    const response = await supertest(endpoint).post("/upload");
+
+    expect(response.status).toBe(401);
+  });
+
+  test("Should get user image with given id", async () => {
+    const user_id = "6439922cf653ecc0c147b448";
+    const response = await supertest(endpoint).get(`/user/${user_id}`);
+
+    expect(response.status).toBe(200);
+  });
+
+  test("Should get product image with given id", async () => {
+    const product_id = "643dd08fac5d12188de2a85e";
+    const response = await supertest(endpoint).get(`/product/${product_id}`);
+
+    expect(response.status).toBe(200);
+  });
+});
