d4594fa46aecd43e1bce434b6ceecdea67f2ab2c
Lucas Santos <lucas.silva1203@outlook.com>
2023-07-05 13:34:29 -0300

    refactor: change interfaces and create new function

--- src/utils/operations.ts
+++ src/utils/operations.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 /**
  * Calculate the value over the percentage.
@@ -18,6 +18,18 @@ export function calculateDiscountedValue(
  * @param str String to check.
  * @returns True if is valid, false if not.
  */
-export function checkIfValidId(str: string): boolean {
+export function checkIfValidId(str: string | Types.ObjectId): boolean {
   return mongoose.Types.ObjectId.isValid(str);
+}
+
+/**
+ * Get an array of products and return the total price.
+ * @param products Array of products.
+ * @returns Total price.
+ */
+export function getTotalFromProducts(products: CartItem[]) {
+  const total = 0;
+  return products
+    .map(({ price, quantity }) => price * quantity)
+    .reduce((acc, value) => acc + value, total);
 }
