88eea85271b17d318117af92660e496e9720a416
Lucas Santos <lucas.silva1203@outlook.com>
2023-07-05 16:32:00 -0300

    fix: paymentMethod validation

--- src/validators/TransactionValidator.ts
+++ src/validators/TransactionValidator.ts
@@ -1,3 +1,11 @@
+//this enum isnt being converted to a js object idk why
+enum PaymentOptions {
+  bankSlip = "Boleto",
+  creditCard = "Cartão de Crédito",
+  bitcoin = "Bitcoin",
+  pix = "Pix",
+}
+
 class TransactionValidator {
   public checkCreate(request_body: TransactionItemDTO): void {
     this.validatePaymentMethod(request_body.paymentMethod);
