039419622963b5944ff6b08de300df6c5d760ceb
Lucas Santos <lucas.silva1203@outlook.com>
2023-08-22 18:39:18 -0300

    chore: add dotenv

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,13 @@ import { ServerModule } from "./server.module";
 
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv" 
 
 async function electroware() {
   await connectDB();
   const app = await NestFactory.create(ServerModule);
+
+  dotenv.config()
   app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
