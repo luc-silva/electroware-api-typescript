bc99d65de5f3ff8c9c17c004012031acca6b3ef7
Lucas Santos <lucas.silva1203@outlook.com>
2023-08-23 10:15:46 -0300

    fix: change connection string format

--- src/middleware/db.ts
+++ src/middleware/db.ts
@@ -3,7 +3,7 @@ const { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASS } = process.env;
 
 export async function connectDB() {
   try {
-    const connectionString = `mongodb://lucasAdmin:lucas1353@mongodb:27017/project_electroware`;
+    const connectionString = `mongodb://user:pass@mongodb:27017/project_electroware`;
     await mongoose.connect(connectionString ).then(() => {
       console.log(`Database connected`);
     });
