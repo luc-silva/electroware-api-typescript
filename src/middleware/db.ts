bf307aaf040c07a0311b60ac3e896b740c02aa9d
Lucas Santos <lucas.silva1203@outlook.com>
2023-08-24 22:31:25 -0300

    refactor: change connection string to cloud database

--- src/middleware/db.ts
+++ src/middleware/db.ts
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
-const { DB_HOST, DB_NAME, DB_PORT, DB_USER, DB_PASS } = process.env;
 
 export async function connectDB() {
   try {
-    const connectionString = `mongodb://user:pass@mongodb1:27017,mongodb2:27018/project_electroware?replicaSet=mongo-cluster`;
+    const connectionString = "mongodb+srv://lucasAdmin:lucas1353@cluster0.oximzo5.mongodb.net/project_electroware?retryWrites=true&w=majority";
     await mongoose.connect(connectionString ).then(() => {
       console.log(`Database connected`);
     });
